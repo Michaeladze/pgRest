4400577bc456ae0767f3cd5843cf0d50909f05ff
michaeladze <michael.kutateladze@yandex.ru>
2020-08-26 13:20:32 +0300

    CORS

--- src/main/java/com/pg/rest/controller/RequestController.java
+++ src/main/java/com/pg/rest/controller/RequestController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:8000")
+@CrossOrigin
 @RequestMapping(path = "/requests")
 public class RequestController {
 
